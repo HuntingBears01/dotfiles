440cc3faa8c363582944481273c6c525ebf899dd
C Martin <cmartin@ronoc.com>
2024-06-29 20:56:48 +0100

    Add extra checks before adding kitty aliases

--- aliases/kitty.sh
+++ aliases/kitty.sh
@@ -4,8 +4,10 @@
 # shellcheck disable=SC1091
 
 if command -v kitty >/dev/null 2>&1; then
-  alias icat="kitten icat"
-  alias diff="kitten diff"
-  alias ssh="kitten ssh"
-  alias rg="rg --hyperlink-format=kitty"
+  if [[ $TERM = 'xterm-kitty' ]]; then
+    alias icat="kitten icat"
+    alias diff="kitten diff"
+    alias ssh="kitten ssh"
+    alias rg="rg --hyperlink-format=kitty"
+  fi
 fi
